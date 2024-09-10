d06385824725f1f1d4791342d73f28dab6cbb5a1
trmaa <trmaayt@gmail.com>
2024-09-10 15:31:58 +0200

    renders

--- include/camera.hpp
+++ include/camera.hpp
@@ -35,8 +35,8 @@ public:
     const glm::vec3& angle() { return this->_angle; }
 
     Camera(const int& w, const int& h)
-        : _position(-22.2f,-65.f, 133.2f), 
-        _angle(glm::vec3(0.4f, 4.9f, 0.0f)), 
+        : _position(-17.2f,-2.5f, -300.2f), 
+        _angle(glm::vec3(0.f, -4.9f, 0.0f)), 
         _direction(0.0f), _speed(100.0f), _far(400.f*w/192) {
         for (int y = 0; y < h; y++) {
             for (int x = 0; x < w; x++) {
